d22e30d982263cb24467ce92804c1597e30999b1
TerminatingCoder101 <ganesha4us1@gmail.com>
2023-04-17 19:51:28 -0700

    Commit with some random changes...

--- script1.js
+++ script1.js
@@ -121,32 +121,45 @@ emailjs.sendMail(serviceId,templateId,params).then((res) => {
   //  moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bxs-moon" : "bx bxs-moon";
 //})
 
+var menuIcon = document.getElementById('menu-icon');
+var navbar = document.querySelector('navbar');
+ 
+menuIcon.onclick = () => {
+    alert('Whats up!');
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
-//         if(top>=offset && top < offset + height) {
-//             navLinks.forEach(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-//             });
-//         };
-//     });
-// };
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-// ScrollReveal({
-//     reset: true,
-//     distance: '80px',
-//     duration: 2000,
-//     delay: 200
-// });
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top>=offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
 
-// ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-// ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'top'});
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'top'});
+
+
